3c43b66bedc21df3d964093a4646b5a5cbfb9050
brunoro7 <bruno.ro7c@gmail.com>
2022-01-09 02:06:55 -0300

    finalmente entregando requisito9, e entendendo pq o 'dlbClick' estava tb chamando a 'click colorida'. Problema solucionado. Indo para o requisito11.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const botaoCriaTarefa = document.getElementById("criar-tarefa");
 const textoInput = document.getElementById("texto-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
+const botaoResetLista = document.getElementById("apaga-tudo");
+const botaoRemoveCompletas = document.getElementById("remover-selecionados");
 
 /** ======= Botão que cria e adiciona tarefa, uma a uma ======= */
 function criaTarefa (){
   
   const cadaTarefa = document.createElement("li");
+  cadaTarefa.className = "";
   cadaTarefa.innerText = textoInput.value;
   listaTarefas.appendChild(cadaTarefa);
   textoInput.value = "";
@@ -15,45 +18,45 @@ botaoCriaTarefa.addEventListener("click", criaTarefa);
 
 /** ======= No clicar no item, o background.color = rgb(128, 128, 128) ======= */
 
-function coloreTarefa (event){
+function coloreTarefa (event) {
   
-  const coloreCadaTarefa = document.getElementsByTagName("li");
-  
-  for( let tarefa of coloreCadaTarefa){
-    
-    tarefa.className = "";
-    event.target.className = "colorida";  
-  }
+  let listaTarefaColorida = document.querySelectorAll(".colorida");
+
+    if(listaTarefaColorida.length === 0){
+      event.target.classList.add("colorida");
+    }
+    else if(listaTarefaColorida.length > 0){
+      let tarefaColorida = document.querySelector(".colorida");
+      tarefaColorida.classList.remove("colorida");
+      event.target.classList.add("colorida");
+    }
 }
-listaTarefas.addEventListener("click", coloreTarefa);
+listaTarefas.addEventListener("click", coloreTarefa)
+
+/** ======= Essa função comentada abaixo, é a forma como foi criada na primeira versão, ela é funcional, até entrar um "double click", que acaba recebendo por 'reflexo' o primeiro clique, que limpava tudo. Deixei para manter o lembrete. ======= */
+
+// function coloreTarefa (event){
+  
+//   const coloreCadaTarefa = document.getElementsByTagName("li");
+  
+//   for( let tarefa of coloreCadaTarefa){
+    
+//     tarefa.className = "";
+//     event.target.className = "colorida";  
+//   }
+// }
+// listaTarefas.addEventListener("click", coloreTarefa);
 
 /** ======= No 'double click' no item, risca o elemento ======= */
 
-function definiTarefaConcluida (event){
-
-  const tarefaConcluida = document.getElementsByTagName("li");
+function definiTarefaConcluida(event){
   
-  for( let definiConcluida of tarefaConcluida){
-  
-  definiConcluida.className = "";
-  event.target.className = "completed";
-  }
+  event.target.classList.toggle("completed");
+ 
 }
 listaTarefas.addEventListener("dblclick", definiTarefaConcluida);
 
-// /** ======= No 'double click' no item, risca o elemento ======= */
-
-// function conferiTarefaConcluida (event){
-  
-  
-// }
-// listaTarefas.addEventListener("dblclick", conferiTarefaConcluida);
-
-
 /** ======= Botão que reseta lista ======= */
-
-const botaoResetLista = document.getElementById("apaga-tudo");
-
 
 function resetListaTarefa (){
   
